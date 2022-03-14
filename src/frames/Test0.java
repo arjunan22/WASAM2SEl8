9002a6c223c46bb937dbeec62438c2f53b7ba2a7
QSP <QSP@QSP>
2022-03-14 21:00:09 +0530

    AutoSuggestions

--- src/frames/Test0.java
+++ src/frames/Test0.java
@@ -7,19 +7,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test0 {
+
 	public static void main(String[] args) {
 
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	driver.findElement(By.xpath("//a[@class='gb_A']")).click();
-	
-	driver.switchTo().frame(0);
-	
-	driver.findElement(By.xpath("//span[text()='Calendar']")).click();
-	
-	
+driver.findElement(By.xpath("//a[@class='gb_A']")).click();	
+
+driver.switchTo().frame(0);
+
+driver.findElement(By.xpath("//span[text()='Calendar']")).click();
 	}
 
 }
