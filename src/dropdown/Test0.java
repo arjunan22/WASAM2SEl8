e6d8f8c1b22d746fb7a5e2e9e9a69ea81f680f37
QSP <QSP@QSP>
2022-03-16 09:55:26 +0530

    dropdown

--- src/dropdown/Test0.java
+++ src/dropdown/Test0.java
@@ -1,6 +1,7 @@
 package dropdown;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,22 +9,38 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+
 public class Test0 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.ebay.com/");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		WebElement ele = driver.findElement(By.id("gh-cat"));
-Select s=new Select(ele);
-s.selectByIndex(0);
-s.selectByValue("550");
-s.selectByVisibleText("Cameras & Photo");
+WebDriver driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.ebay.com/");
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+WebElement dropdown = driver.findElement(By.id("gh-cat"));
+
+Select s=new Select(dropdown);
+s.selectByIndex(4);
+s.selectByValue("2984");
+s.selectByVisibleText("Crafts");
+
+s.deselectAll();
+
+List<WebElement> alloptions = s.getOptions();
+System.out.println(alloptions.size());
+
+for(WebElement op:alloptions) {
+	System.out.println(op.getText());
+}
+
+
+
+Thread.sleep(2000);
 
 driver.close();
+
 
 	}
 
